db66fc6758ff20ecaa6dff8551be66cec6528c4e
kristinn93 <kristinnj13@ru.is>
2014-05-07 17:20:48 +0000

    yolo!

---
+++ HvadSagdiHannSolution/HvadSagdiHann/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HvadSagdiHann.Models
+{
+    public class Movie
+    {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string ImdbUrl { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+		public Movie( )
+		{
+			DateOfBirth = DateTime.Now;
+		}
+	}
+}
