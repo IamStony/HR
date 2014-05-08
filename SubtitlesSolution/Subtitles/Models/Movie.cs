a74d5613efef483acf40f0390cfc78ee7f5a2452
steinara13@ru.is <steinara13@ru.is>
2014-05-08 22:50:32 +0000

    AddedModels

---
+++ SubtitlesSolution/Subtitles/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Subtitles.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ImdbUrl { get; set; }
+        //public int LanguageID { get; set; }
+        public string Srt { get; set; }
+    }
+}
