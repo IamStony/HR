d4f64488dbe84cbfbdc53167a6fd24f5cee2ce12
kristinn93 <kristinnj13@ru.is>
2014-05-09 15:56:45 +0000

    yo

---
+++ SubtitlesSolution/Subtitles/Models/game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Subtitles.Models
+{
+	public class game
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int MaxPlayers { get; set; }
+		public int MinPlayers { get; set; }
+	}
+}
