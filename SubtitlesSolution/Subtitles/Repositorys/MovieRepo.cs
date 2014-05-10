e4f46b7bfa9d32bc88af78b390f4ff495fe0705d
kristinn93 <kristinnj13@ru.is>
2014-05-10 12:05:19 +0000

    yo

--- SubtitlesSolution/Subtitles/Repositorys/MovieRepo.cs
+++ SubtitlesSolution/Subtitles/Repositorys/MovieRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Subtitles.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,17 @@ namespace Subtitles.Repositorys
 {
 	public class MovieRepo
 	{
+		public Movie GetAllMovies()
+		{
+			var a = new Movie();
+			a.ID = 1;
+			a.Name = "KLISTINN";
+			return a;
+		}
+
+		public void AddMovie(Movie m)
+		{
+			 //Context.
+		}
 	}
 }
