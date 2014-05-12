ff0e543dea39783e607d4f3659f898e5c8fc673a
kristinn93 <kristinnj13@ru.is>
2014-05-12 15:12:14 +0000

    YO

--- SubtitlesSolution/Subtitles/Controllers/HomeController.cs
+++ SubtitlesSolution/Subtitles/Controllers/HomeController.cs
@@ -61,22 +61,6 @@ namespace Subtitles.Controllers
 			// redirect back to the index action to show the form once again
 			return RedirectToAction("Index");
 		}
-		[HttpPost]
-		public JsonResult Upload()
-		{
-			for (int i = 0; i < Request.Files.Count; i++)
-			{
-				HttpPostedFileBase file = Request.Files[i]; //Uploaded file
-				//Use the following properties to get file's name, size and MIMEType
-				int fileSize = file.ContentLength;
-				string fileName = file.FileName;
-				string mimeType = file.ContentType;
-				System.IO.Stream fileContent = file.InputStream;
-				//To save file, use SaveAs method
-				file.SaveAs(Server.MapPath("~/App_Data") + fileName); //File will be saved in application root
-			}
-			return Json("Uploaded " + Request.Files.Count + " files");
-		}
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
