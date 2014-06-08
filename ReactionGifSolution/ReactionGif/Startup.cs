6b553b2b968126dcd83fda777e07328375e1591e
Steinar Árnason <steinara13@ru.is>
2014-06-08 15:37:04 +0000

    Added basic project & goals

---
+++ ReactionGifSolution/ReactionGif/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactionGif.Startup))]
+namespace ReactionGif
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
